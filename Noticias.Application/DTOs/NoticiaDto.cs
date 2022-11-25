e87f1bd023bffdb2a9a56d24f7d1c039d21f92bd
Eduarda Soares <edu.soaresferreira@outlook.com.br>
2022-11-25 15:06:38 -0300

    messages updated

--- Noticias.Application/DTOs/NoticiaDto.cs
+++ Noticias.Application/DTOs/NoticiaDto.cs
@@ -6,11 +6,11 @@ public class NoticiaDto
 {
     [Required(ErrorMessage = "O titulo não pode ser vazio")]
     [MaxLength(80, ErrorMessage = "O titulo deve ter no máximo 80 caracteres")]
-    [MinLength(3, ErrorMessage = "o titulo deve ter no mínimo 3 caracteres")]
+    [MinLength(5, ErrorMessage = "o titulo deve ter no mínimo 3 caracteres")]
     public string Titulo { get; set; } = null!;
     
     [Required(ErrorMessage = "O texto não pode ser vazio")]
     [MaxLength(5000, ErrorMessage = "O texto deve ter no máximo 5000 caracteres")]
-    [MinLength(5, ErrorMessage = "o titulo deve ter no mínimo 5 caracteres")]
+    [MinLength(5, ErrorMessage = "O texto deve ter no mínimo 5 caracteres")]
     public string Texto { get; set; } = null!;
 }
--- Noticias.Application/DTOs/UpdateNoticiaDto.cs
+++ Noticias.Application/DTOs/UpdateNoticiaDto.cs
@@ -9,11 +9,11 @@ public class UpdateNoticiaDto
     
     [Required(ErrorMessage = "O titulo não pode ser vazio")]
     [MaxLength(80, ErrorMessage = "O titulo deve ter no máximo 80 caracteres")]
-    [MinLength(3, ErrorMessage = "o titulo deve ter no mínimo 3 caracteres")]
+    [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 3 caracteres")]
     public string Titulo { get; set; } = null!;
     
     [Required(ErrorMessage = "O texto não pode ser vazio")]
-    [MaxLength(3000, ErrorMessage = "O texto deve ter no máximo 3000 caracteres")]
-    [MinLength(3, ErrorMessage = "o titulo deve ter no mínimo 3 caracteres")]
+    [MaxLength(5000, ErrorMessage = "O texto deve ter no máximo 3000 caracteres")]
+    [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 3 caracteres")]
     public string Texto { get; set; } = null!;
 }
