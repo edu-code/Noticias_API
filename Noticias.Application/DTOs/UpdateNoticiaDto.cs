35a7fe7a7893f27d5ecc67754dccd54aca540e63
Eduarda Soares <edu.soaresferreira@outlook.com.br>
2022-11-25 15:09:06 -0300

    updated

--- Noticias.Application/DTOs/UpdateNoticiaDto.cs
+++ Noticias.Application/DTOs/UpdateNoticiaDto.cs
@@ -9,11 +9,11 @@ public class UpdateNoticiaDto
     
     [Required(ErrorMessage = "O titulo não pode ser vazio")]
     [MaxLength(80, ErrorMessage = "O titulo deve ter no máximo 80 caracteres")]
-    [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 3 caracteres")]
+    [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 5 caracteres")]
     public string Titulo { get; set; } = null!;
     
     [Required(ErrorMessage = "O texto não pode ser vazio")]
     [MaxLength(5000, ErrorMessage = "O texto deve ter no máximo 3000 caracteres")]
-    [MinLength(5, ErrorMessage = "O titulo deve ter no mínimo 3 caracteres")]
+    [MinLength(5, ErrorMessage = "O texto deve ter no mínimo 5 caracteres")]
     public string Texto { get; set; } = null!;
 }
